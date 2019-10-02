86293232b65759d3999d3f891be5109d2e6f7593
Paweł Balbierz <55387853+snufkinpl@users.noreply.github.com>
2019-10-02 11:18:01 +0200

    my first repository

---
+++ test.py
@@ -0,0 +1,23 @@
+digits = {
+    "1": "Jeden",
+    "2": "Dwa",
+    "3": "Trzy",
+    "4": "Cztery",
+    "5": "Pięć",
+    "6": "Sześć",
+    "7": "Siedem",
+    "8": "Osiem",
+    "9": "Dziewięć",
+    "0": "Zero",
+    " ": ""
+              }
+result = ""
+phone = input("Wprowadź numer telefonu: ")
+for character in phone:
+    result += digits.get(character, "") + " "
+print(result)
+
+
+print("*" * 30)
+print("KONIEC PROGRAMU.")
+
